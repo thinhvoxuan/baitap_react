0094705aaf9b74a64fc8e78cd5fbc8d3dda3d7f7
thinhvoxuan <thinhvoxuan@gmail.com>
2017-05-13 00:08:43 +0700

    update router and remove unused variable

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import {
-  HashRouter,
-  Route,
-  Link
+  BrowserRouter as Router,
+  Route
 } from 'react-router-dom';
 import Home from './Home'
 import Member from './Member'
 import Group from './Group'
 import Photo from './Photo'
-import Profile from './Profile'
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
+      <Router>
         <div className="container">
             <Route exact path='/' component={Home} />
             <Route exact path='/member' component={Member} />
             <Route path='/group' component={Group} />
             <Route path='/photo' component={Photo} />
         </div>
-      </HashRouter>
+      </Router>
     );
   }
 }
